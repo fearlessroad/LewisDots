a31d9b977480976334b4c0b970547f5048de3e97
Abigail <acdommer@gmail.com>
2017-01-18 12:03:44 -0800

    in the middle of working up radius coordinates to make bonds prettier

--- graphics_helper.js
+++ graphics_helper.js
@@ -1,20 +1,27 @@
 var bondedMatrix = null;
+var bondedAtomSVG = null;
+var currentAtom = null; 
+var currentAtomSVG = null; 
+var currentBondSVG = null; 
 var extractBondedAtomCoordinates = function(){
 	console.log('inside extract bonded atom coords')
 	//function function function 
-	for (var i = 0; i<findObjectInObjectArrayWithId(selectedElement.id).bonds.length; i++){ // finding the bonds of the ATOM that is currently being moved(selectedElement)
-		var moveElementBondId = findObjectInObjectArrayWithId(selectedElement.id).bonds[i] // this should return the id of the bond itself. 
-		//now you need to set currentBond to THIS BOND
-		currentBond = document.getElementById(moveElementBondId);
+	currentAtom = findObjectInObjectArrayWithId(selectedElement.id);
+	currentAtomSVG = document.getElementById(selectedElement.id);
+	for (var i = 0; i<currentAtom.bonds.length; i++){ // finding the bonds of the ATOM that is currently being moved(selectedElement)
+		var moveElementBondId = currentAtom.bonds[i] // this should return the id of the bond itself. 
+		//now you need to set currentBondSVG to THIS BOND
+		currentBondSVG = document.getElementById(moveElementBondId);
 		//The bond itself, when searched for its "bond" array in the ObjectArray, should return TWO atom ids. 
-		var moveElementBondedAtomId = extractBondedAtomId(moveElementBondId); // this should return the Bonded Atom Id AND a designation of whether its part of the x1 or x2 side of the line, so we now have that Id and the Bond Id AND we know which side of the line svg we are dealing with
+		var moveElementBondedAtomId = extractBondedAtomId(moveElementBondId);// this should return the Bonded Atom Id AND a designation of whether its part of the x1 or x2 side of the line, so we now have that Id and the Bond Id AND we know which side of the line svg we are dealing with
+		bondedAtomSVG = document.getElementById(moveElementBondedAtomId[2]); 
 		//console.log("function works"+extractBondedMatrix(moveElementBondedAtomId));
 		//console.log(bondedMatrix); // the matrix extracted from the bonded Atom Id
 		//console.log("this is the bonded atom matrix: "+document.getElementById(moveElementBondedAtomId[2]).getAttributeNS(null, "transform"));//this logs the elements transform matrix
-		console.log("the bonded atom current x,y coordinates are: "+document.getElementById(moveElementBondedAtomId[2]).x.baseVal[0].value+","+document.getElementById(moveElementBondedAtomId[2]).y.baseVal[0].value)
+		console.log("the bonded atom current x,y coordinates are: "+bondedAtomSVG.x.baseVal[0].value+","+bondedAtomSVG.y.baseVal[0].value)
 		//One will be the selected object (selectedElement.id), the other will be the attached atom. We need to grab the attached atom x,y coordinates by looking it up with document.getElementById (the one that is NOT selectedElement)
-		currentBond.setAttributeNS(null, moveElementBondedAtomId[0], selectedElement.x.baseVal[0].value+currentMatrix[4]);
-		currentBond.setAttributeNS(null, moveElementBondedAtomId[1], selectedElement.y.baseVal[0].value+currentMatrix[5]);
+		currentBondSVG.setAttributeNS(null, moveElementBondedAtomId[0], selectedElement.x.baseVal[0].value+currentMatrix[4]);
+		currentBondSVG.setAttributeNS(null, moveElementBondedAtomId[1], selectedElement.y.baseVal[0].value+currentMatrix[5]);
 	}
 }
 var extractBondedMatrix = function(moveElementBondedAtomId){
@@ -49,4 +56,28 @@ var extractMatrix = function(atomId){
 			bondedMatrix[i] = parseFloat(bondedMatrix[i]);
 		}
 		return bondedMatrix;
+}
+var radius1 = null; 
+var radius2 = null;
+var x1 = null;
+var y1 = null;
+var x2 = null;
+var y2 = null; 
+var manipulateFinishedBond = function(){
+	if (extractBondedAtomId[0]=="x2"){
+		radius2 = getRadius(bondedAtomSVG);
+		radius1 = getRadius(currentAtomSVG);
+		x1 = 
+	}
+	else{
+		radius2 = getRadius(currentAtomSVG);
+		radius1 = getRadius(bondedAtomSVG);
+	}
+}
+var getRadius = function(SVG){
+	var rad = (((SVG.scrollHeight/2)^2)+((SVG.scrollWidth/2)^2))^(1/2);
+	return rad; 
+}
+var determineRadialCoordinate = function(radius, SVG){
+	if ()
 }
