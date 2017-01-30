e8020792084fd53c456beb63864bc8d984c1dd11
Abigail <acdommer@gmail.com>
2017-01-30 10:54:17 -0800

    THE BONDS ARE FINALLY PRETTY OMGGGGG

--- graphics_helper.js
+++ graphics_helper.js
@@ -20,21 +20,35 @@ var handleBondElementMovement = function(){
 	}
 }
 var setNewCoordinates = function(){
-	for (key in currentBondObject.bonds[0]){
-		var el = document.getElementById(currentBondObject.bonds[0][key]);
-		var matrix = getMatrix(el);
-		var firstSVG = document.getElementById(currentBondObject.bonds[0]["x1"]);
-		var secondSVG = document.getElementById(currentBondObject.bonds[0]["x2"]);
-		console.log("first is: "+ firstSVG);
-		currentBondSVG.setAttributeNS(null, key, el.x.baseVal[0].value+(el.scrollWidth/2)+matrix[4]);
-//determining if the current bond is x1 or x2
-		if (key == "x1"){
-			currentBondSVG.setAttributeNS(null, "y1", el.y.baseVal[0].value-(el.scrollHeight/3)+matrix[5]);	
-		}
-		else{
-			currentBond.setAttributeNS(null, "y2", el.y.baseVal[0].value+matrix[5]-(el.scrollHeight/3));
-		}	
+	var firstSVG = document.getElementById(currentBondObject.bonds[0]["x1"]);
+	var firstMatrix = getMatrix(firstSVG);
+	var secondSVG = document.getElementById(currentBondObject.bonds[0]["x2"]);
+	var secondMatrix = getMatrix(secondSVG);
+	var firstX = firstSVG.x.baseVal[0].value+(firstSVG.scrollWidth/2)+firstMatrix[4];
+	var firstY = firstSVG.y.baseVal[0].value-(firstSVG.scrollHeight/3)+firstMatrix[5];
+	var secondX = secondSVG.x.baseVal[0].value+(secondSVG.scrollWidth/2)+secondMatrix[4];
+	var secondY = secondSVG.y.baseVal[0].value-(secondSVG.scrollHeight/3)+secondMatrix[5];
+	var firstRadius = getRadius(firstSVG);
+	var secondRadius = getRadius(secondSVG);
+	if (firstX>secondX){
+		console.log('firstX >second X');
+		currentBondSVG.setAttributeNS(null, "x1", firstX-(1/2)*firstRadius*Math.cos(Math.atan((secondY-firstY)/(secondX-firstX))));
+		currentBondSVG.setAttributeNS(null,"y1", firstY-(1/2)*firstRadius*Math.sin(Math.atan((secondY-firstY)/(secondX-firstX))));
+		currentBondSVG.setAttributeNS(null, "x2", secondX+(1/2)*secondRadius*Math.cos(Math.atan((firstY-secondY)/(firstX-secondX))));
+		currentBondSVG.setAttributeNS(null, "y2", secondY+(1/2)*secondRadius*Math.sin(Math.atan((firstY-secondY)/(firstX-secondX))));
 	}
+	else{
+		console.log('secondX > firstX');
+		currentBondSVG.setAttributeNS(null, "x1", firstX+(1/2)*firstRadius*Math.cos(Math.atan((secondY-firstY)/(secondX-firstX))));
+		currentBondSVG.setAttributeNS(null,"y1", firstY+(1/2)*firstRadius*Math.sin(Math.atan((secondY-firstY)/(secondX-firstX))));
+		currentBondSVG.setAttributeNS(null, "x2", secondX-(1/2)*secondRadius*Math.cos(Math.atan((firstY-secondY)/(firstX-secondX))));
+		currentBondSVG.setAttributeNS(null, "y2", secondY-(1/2)*secondRadius*Math.sin(Math.atan((firstY-secondY)/(firstX-secondX))));
+
+	}
+	//currentBondSVG.setAttributeNS(null, "x1", firstX);
+	//currentBondSVG.setAttributeNS(null, "x2", secondX);
+	//currentBondSVG.setAttributeNS(null, "y1", firstY);
+	//currentBondSVG.setAttributeNS(null, "y2", secondY);
 }
 var getMatrix = function(SVG){
 	bondedMatrix = SVG.getAttributeNS(null, "transform").slice(7, -1).split(' ');
@@ -75,10 +89,10 @@ var extractMatrix = function(atomId){
 // 		radius1 = getRadius(bondedAtomSVG);
 // 	}
 // }
-// var getRadius = function(SVG){
-// 	var rad = (((SVG.scrollHeight/2)^2)+((SVG.scrollWidth/2)^2))^(1/2);
-// 	return rad; 
-// }
+var getRadius = function(SVG){
+	var rad = (((SVG.scrollHeight/2)^2)+((SVG.scrollWidth/2)^2))^(1/2);
+	return rad; 
+}
 // var determineRadialCoordinate = function(radius, SVG){
 // 	var coords = getRadialCoordinates(radius, SVG);
 // 	if (currentAtomSVG.x.baseVal[0].value > bondedAtomSVG.x.baseVal[0].value){
